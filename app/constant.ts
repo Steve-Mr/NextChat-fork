cb360d2013a211d28eb257b36a8ba48e885a93ee
Maary <24504742+Steve-Mr@users.noreply.github.com>
2025-06-07 11:50:20 +0800

    remove deprecated model

--- app/constant.ts
+++ app/constant.ts
@@ -547,7 +547,6 @@ const googleModels = [
   "gemini-2.0-pro-exp",
   "gemini-2.0-pro-exp-02-05",
   "gemini-2.5-flash-preview-05-20",
-  "gemini-2.5-pro-experimental-03-25",
   "gemini-2.5-pro-preview-06-05",
 ];
 
